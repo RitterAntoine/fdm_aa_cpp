8866ae64a3538bea8bf7766d1e41b17b4d89be40
RitterAntoine <antoine.ritter@viacesi.fr>
2024-01-25 12:46:45 +0100

    feat: local lib working

--- src/main/fdm_aa_cpp.cpp
+++ src/main/fdm_aa_cpp.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "add.h"
+#include "subtract.h"
 
 int main() {
-    std::cout << "Program is running" << std::endl;
+    int a = 5;
+    int b = 3;
+
+    std::cout << "Sum: " << add(a, b) << std::endl;
+    std::cout << "Difference: " << subtract(a, b) << std::endl;
+
     return 0;
 }
