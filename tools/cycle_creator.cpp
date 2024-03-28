e4b2d6fc3c9e7c77ad04fda04002c8e9e5ca25df
RitterAntoine <antoine.ritter@viacesi.fr>
2024-03-28 11:21:17 +0100

    feat: tools updated

---
+++ tools/cycle_creator.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include <cglib/cycle.h>
+
+// This program will extract the cycles from a given graph. The graph will be in the argument list from a file.
+// The program will write the cycles and all the information about the cycles to the standard output(document name second argument).
+// The format of the input file will be only the values in order, as follows:
+// 3 3
+// 1 1 1
+// 0 1 0
+// 1 0 1
+
+int main(int argc, char* argv[])
+{
+    // Check if the correct number of arguments was passed (2 arguments are expected)
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " <input_file> <output_file>" << std::endl;
+        return 1;
+    }
+
+    // Use the first argument as the input file
+    std::string input_file = argv[1];
+    // Use the second argument as the output file
+    std::string output_file = argv[2];
+
+    // Check if the input file exists
+    std::ifstream file(input_file);
+    if (!file) {
+        std::cerr << "Error: file " << input_file << " not found" << std::endl;
+        return 1;
+    }
+
+    // Read the size of the graph from the input file
+    int cols, rows;
+    file >> cols >> rows;
+
+    // Initialize the graph with the correct size
+    Eigen::MatrixXi graph(rows, cols);
+
+    // Read the graph from the input file
+    for (int i = 0; i < graph.rows(); i++) {
+        for (int j = 0; j < graph.cols(); j++) {
+            file >> graph(i, j);
+        }
+    }
+
+    // Convert the graph to a Graph object
+    // First the grid
+    Eigen::Array<int, 2, 1> cell_2dcount(2);
+    cell_2dcount << cols, rows;
+    Eigen::Array<int, 2, 1> origin(2);
+    origin << 0, 0;
+    float cell_sides_length = 1;
+    Grid grid(cell_2dcount, origin, cell_sides_length);
+
+    // Then the scalar field
+    Eigen::Array<double, Eigen::Dynamic, 1> flattened_scalar_field = graph.cast<double>().matrix().transpose();
+
+    Graph res = grid2_contour(flattened_scalar_field, cell_2dcount, grid);
+
+    Eigen::Array<float, Eigen::Dynamic, 2> list_point = res.getListPoint();
+    Eigen::Array<int, Eigen::Dynamic, 2> list_adjacency = res.getListAdjacency();
+
+    // Create the cycles from the graph
+    Cycle cycle = create_from_graph(res);
+
+    // Write the cycles to the output file
+    save_cycle(output_file, cycle);
+
+    return 0;
+}
